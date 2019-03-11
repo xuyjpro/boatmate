9778c1a48fa363530a89f2023f6b1fcd8809b5c2
XUYUJIE <892575153@qq.com>
2019-03-11 22:18:36 +0800

    with token help

--- src/com/xuyj/boatmate/action/GetSchoolHelps.java
+++ src/com/xuyj/boatmate/action/GetSchoolHelps.java
@@ -4,13 +4,19 @@ import com.xuyj.boatmate.dao.ISchoolHelpDAO;
 
 public class GetSchoolHelps extends BaseAction {
 
+	private String token;
 	private int currentPage=1;
 	private int pageSize=20;
 	@Override
 	public String post() {
 		// TODO Auto-generated method stub
 		ISchoolHelpDAO dao=(ISchoolHelpDAO) getApplicationContext().getBean("schoolHelpDAO");
-		rb=dao.getList(currentPage, pageSize);
+		if(token==null){
+			rb=dao.getList(currentPage, pageSize);
+
+		}else{
+			rb=dao.getList(token, currentPage, pageSize);
+		}
 		
 		return SUCCESS;
 	}
@@ -25,6 +31,12 @@ public class GetSchoolHelps extends BaseAction {
 	}
 	public void setPageSize(int pageSize) {
 		this.pageSize = pageSize;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
 	}
 
 }
